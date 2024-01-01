37cf65ebe2c8f3697f7d7a49612582bfda3ad879
Марк дядченко <118738936+ggoooooooooooooooooo@users.noreply.github.com>
2024-01-01 21:28:52 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://api.naga.ac/v1"
+API_KEY = "YOUR API" 
+APP_ID = "H35GLT-UY9Y35LAR7"
+
+promt = """Улудши запрос по критериям уточнения,  более специализированные команды или запросы"""
